7cf8109c6b983b20e3f3b9d65d128d7879efa67d
Shravan Jambukesan <Shravan.j97@gmail.com>
2015-10-14 21:12:20 -0500

    Update jDiskStressTester.java

--- jDiskStressTester.java
+++ jDiskStressTester.java
@@ -13,7 +13,7 @@ public class jDiskStressTester
 	private static String verbose;
 	private static boolean isVerbose;
 	public static void main(String[] args)
-	{
+	  {
 		boolean b = true;
 		Scanner scan = new Scanner(System.in);
 		long numFiles = 0;
@@ -25,54 +25,54 @@ public class jDiskStressTester
 		System.out.print("Would you like verbose output <yes/no>?: ");
 		verbose = scan.next();
 		if(verbose.equals("yes"))
-		{
+		  {
 			isVerbose = true;
-		}
+		  }
 		else
-		{
+		  {
 			isVerbose = false;
-		}
+		  }
 		System.out.println("Running...");
 		while(b)
-		{
+		  {
 			boolean run = fileCreator(numFiles);
 			if(run)
-			{
+			  {
 				b = false;
-			}
-		}
-	}
+			  }
+		  }
+	  }
 
 	public static boolean fileCreator(long numFiles)
-	 {
+	  {
 		boolean done = false;
 		String str = "";
 		long start;
 		start = System.currentTimeMillis();
 		for(long x = 1; x <= numFiles; x++)
-		 {
+		  {
 			try
-        		 {
+        		  {
             			str = "jDiskStressTestFile_" + (long) (x) + ".txt";
             			Thread.sleep(1);
             			PrintWriter p = new PrintWriter(str);
             			if(isVerbose)
-            			 {
+            			  {
             				System.out.println("File " + str +  " was created");
-            			 }
+            			  }
             			if(x == numFiles)
-            			 {
+            			  {
             				long end = System.currentTimeMillis();
             				double time = (end - start) / 1000;
             				System.out.println(numFiles + " files were generated in " + time + " seconds");
             				done = true;
-            			 }
-         		 }
+            			  }
+         		  }
          		catch (Exception e)
-         		 {
+         		  {
             			System.out.print("Error " + e);
-         		 }
-		 }
+         		  }
+		  }
 		return done;
-	 }	
+	  }	
 }
